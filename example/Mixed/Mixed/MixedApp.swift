f915fbdd97cf12994c7cc56aacfec38442060641
Jian Fang <jian_fang@intuit.com>
2024-10-22 12:46:34 -0700

    add emtpy xcode project

---
+++ example/Mixed/Mixed/MixedApp.swift
@@ -0,0 +1,17 @@
+//
+//  MixedApp.swift
+//  Mixed
+//
+//  Created by Jian Fang on 10/22/24.
+//
+
+import SwiftUI
+
+@main
+struct MixedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
